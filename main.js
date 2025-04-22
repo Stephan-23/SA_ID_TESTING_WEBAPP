3c35283c0d607c01532f3e0b0a239348805ec631
Thembinkosi <thembinkosidladla318@gmail.com>
2025-04-22 13:45:57 +0200

    set-up for the project will index.html

---
+++ main.js
@@ -0,0 +1,9 @@
+//get the input
+//check if the iput is filled
+//throw error msg in the msg space
+//if the input is filled check for the valiadation
+//is is digit only
+//if not throw the error
+//the size is 13
+//if less or more 
+//throw the error
