b97351685fee7c46faad4d298277b46429f277fa
Thembinkosi <thembinkosidladla318@gmail.com>
2025-04-22 14:30:07 +0200

    add validation for entered id and display message if not entered

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+const idInput = document.querySelector('#idInput')
+const btn = document.querySelector('#btn');
+const msg = document.querySelector('#msg');
+
+btn.addEventListener('click', onClick);
+
+function onClick(e){
+    //console.log('button click')
+    if( idInput.value === ''){                           //check if the iput is filled
+        msg.innerHTML = "CANNOT TEST THE EMPTY ID"      //throw error msg in the msg space
+      //alert('can not test empty id')
+      //remove the msg after 4sec
+      setTimeout(() => msg.remove(), 4000)
+    }
+}
+
+
+
 //get the input
 //check if the iput is filled
 //throw error msg in the msg space
