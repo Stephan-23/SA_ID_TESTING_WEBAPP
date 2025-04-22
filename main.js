9bee05a48919a59e00acf88608900cd9360fe7d1
Thembinkosi <thembinkosidladla318@gmail.com>
2025-04-22 15:37:11 +0200

    refact: the message function

--- main.js
+++ main.js
@@ -4,19 +4,37 @@ const msg = document.querySelector('#msg');
 
 btn.addEventListener('click', onClick);
 
+
+
+
+function removeMsg(){
+    setTimeout(() => msg.remove(), 4000)//remove the msg after 4sec
+}
+
 function onClick(e){
     //console.log('button click')
     if( idInput.value === ''){                           //check if the iput is filled
         msg.innerHTML = "CANNOT TEST THE EMPTY ID"      //throw error msg in the msg space
       //alert('can not test empty id')
-      
-      setTimeout(() => msg.remove(), 4000)//remove the msg after 4sec
+       removeMsg();
+      //setTimeout(() => msg.remove(), 4000)//remove the msg after 4sec
     } else {
                // Check if the input contains only digits (no letters or special characters)
             if (!/^\d+$/.test(idInput.value)) {
                 msg.textContent = 'Invalid ID: Please enter only numbers.';  // Error message for invalid input
                 msg.style.color = 'red';  // Set message color to red for error
+                removeMsg();
             } 
+            else if(idInput.value.length !== 13 ){
+                msg.innerHTML = "WRONG ID" 
+                removeMsg();
+            }
+
+
+            if(idInput.value.length !== 13){
+                msg.innerHTML = "Check the length of your id make sure it has 13 digit"
+                removeMsg();
+            }
   
         }
 }
