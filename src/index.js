77c4fdfb13cf8288f70c9f257c39ab5be5967f70
Thembinkosi <thembinkosidladla318@gmail.com>
2025-05-05 14:38:31 +0200

    feat: add the simple get methods to render the login page and the signup page

--- src/index.js
+++ src/index.js
@@ -6,10 +6,26 @@ const bcrypt = require('bcrypt');
 
 //create the express application
 const app = express();
+app.use(express.json());   //convert te data to the JSON
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded({ extended: false }))
+//set ejs as view engine, go to for view and render the ejs files as html
+app.set('view engine', 'ejs')
+
+app.get(('/'), (req, res)=>{
+    res.render('Login')// render the login page.
+})
+
+app.get(('/Signup'), (req, res)=>{
+    res.render('Signup')// render the Signup page.
+})
+
 
 
 //choose the port to run the server
-const port = 50000;
+const port = 5000;
 app.listen(port , ()=>{
     console.log(`Server is running on port: ${port}`);
 })
